508c83db964cb3500d36fe05874189be9a401c60
kramak14 <krishnanramakrishnan14@gmail.com>
2019-01-28 11:18:15 -0800

    Trigger to insert student grade data
    BEFORE INSERT trigger that is written to insert data into the student_grade table. After compiling the trigger, we write an INSERT statement to insert student_grade data such as first_name, last_name, course_name, and student_grade as specified in the trigger.

---
+++ tr_insertstudentgrade.sql
@@ -0,0 +1,71 @@
+CREATE OR REPLACE TRIGGER TR_INSERTSTUDENTGRADE
+BEFORE INSERT ON STUDENTS
+FOR EACH ROW
+/*
+TRIGGER_NAME            : TR_INSERTSTUDENT
+PACKAGE_NAME            : STANDARD
+APPLICATION NAME        : PERSONAL PROJECT
+OWNER                   : KRIS
+AUTHOR                  : KRISHNAN
+DATE WRITTEN            : 1-SEP-2018
+*****************************************************************************
+1-SEP-2018  KRIS  -  Krishnan       Created
+-----------------------------------------------------------------------------
+Date       User    -------Name-------   -------  Comments  -------------------
+*****************************************************************************
+*/
+
+/*
+*****************************************************************************
+Section : 01 - Declare variables
+*****************************************************************************
+*/
+  DECLARE
+        PROCESS_NAME              VARCHAR2(250);
+	LOCATOR_TEXT              VARCHAR2(250);
+  BEGIN
+   /*
+   *****************************************************************************
+   SECTION     : 02  -  Initialize Variables and select student_id from Students
+                        table
+   *****************************************************************************
+   */
+	PROCESS_NAME              := 'TR_INSERTSTUDENTGRADE';
+	LOCATOR_TEXT              := '';
+    /*
+   *****************************************************************************
+   SECTION     : 03  -  Insert a new row into the Students table having a local
+                        exception.
+   *****************************************************************************
+   */
+   BEGIN	
+   :new.student_firstname  := UPPER(:new.student_firstname);
+   :new.student_lastname   := UPPER(:new.student_lastname);
+   :new.student_coursename := INITCAP(:new.student_coursename);
+   :new.letter_grade       := FN_GRADERANGE(:new.student_score);
+   END;
+    /*
+   *****************************************************************************
+   SECTION     : 04  -  Continue executing program after local exception
+   *****************************************************************************
+   */
+   /*
+   **********************************************************************
+   SECTION  : 99
+   Exception Handling
+   **********************************************************************
+   */
+    EXCEPTION
+        /*
+        ******************************************************************
+        SECTION   :  99-Z
+        -
+        Handle Database level exceptions WHEN OTHERS
+        Insert a row into the RDC_EXCEPTION_LOG
+        -
+        ******************************************************************
+        */
+        WHEN OTHERS THEN
+         DBMS_OUTPUT.put_line(SQLCODE);
+         DBMS_OUTPUT.put_line(SQLERRM(SQLCODE));
+END TR_INSERTSTUDENTGRADE;
