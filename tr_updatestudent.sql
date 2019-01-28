b68b4f0e9b6c94370d9bb5b8c85916c2b60984f1
kramak14 <krishnanramakrishnan14@gmail.com>
2019-01-28 11:13:31 -0800

    Trigger on updating student data
    This trigger will update the students table. First, we will compile the trigger, then write the update statement to update appropriate data in the students table.

---
+++ tr_updatestudent.sql
@@ -0,0 +1,70 @@
+CREATE OR REPLACE TRIGGER TR_UPDATESTUDENT
+BEFORE UPDATE ON STUDENTS
+FOR EACH ROW
+/*
+TRIGGER_NAME            : TR_UPDATESTUDENT
+PACKAGE_NAME            : STANDARD
+APPLICATION NAME        : PERSONAL PROJECT
+OWNER                   : KRIS
+AUTHOR                  : KRISHNAN
+DATE WRITTEN            : 2-SEP-2018
+*****************************************************************************
+1-SEP-2018  KRIS  -  Krishnan       Created
+-----------------------------------------------------------------------------
+Date       User    -------Name-------   -------  Comments  -------------------
+*****************************************************************************
+*/
+
+/*
+*****************************************************************************
+Section : 01 - Declare variables
+*****************************************************************************
+*/
+  DECLARE
+        PROCESS_NAME              VARCHAR2(250);
+	LOCATOR_TEXT              VARCHAR2(250);
+  BEGIN
+   /*
+   *****************************************************************************
+   SECTION     : 02  -  Initialize Variables and select student_id from Students
+                        table
+   *****************************************************************************
+   */
+	PROCESS_NAME              := 'TR_UPDATESTUDENT';
+	LOCATOR_TEXT              := '';
+    /*
+   *****************************************************************************
+   SECTION     : 03  -  Update a new row into the Students table having a local
+                        exception.
+   *****************************************************************************
+   */
+   BEGIN	
+   :new.student_firstname  := UPPER(:old.student_firstname);
+   :new.student_lastname   := UPPER(:old.student_lastname);
+   :new.student_coursename := INITCAP(:old.student_coursename);
+   END;
+    /*
+   *****************************************************************************
+   SECTION     : 04  -  Continue executing program after local exception
+   *****************************************************************************
+   */
+   /*
+   **********************************************************************
+   SECTION  : 99
+   Exception Handling
+   **********************************************************************
+   */
+    EXCEPTION
+        /*
+        ******************************************************************
+        SECTION   :  99-Z
+        -
+        Handle Database level exceptions WHEN OTHERS
+        Insert a row into the RDC_EXCEPTION_LOG
+        -
+        ******************************************************************
+        */
+        WHEN OTHERS THEN
+         DBMS_OUTPUT.put_line(SQLCODE);
+         DBMS_OUTPUT.put_line(SQLERRM(SQLCODE));
+END TR_INSERTSTUDENT;
